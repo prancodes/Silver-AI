b30d73f24fb4ea3c399618e3f457053b03bba2b9
pushkarsingh09 <162876980+pushkarsingh09@users.noreply.github.com>
2025-01-08 18:39:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -8,6 +8,38 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
+    <?php
+require_once 'vendor/autoload.php';
+
+
+$clientID = '1034298807661-bgjk5c085fjpakvidkuqsuvuijvjhogm.apps.googleusercontent.com';
+$clientSecret = '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';
+$redirectUri = 'http://localhost/Login/silver.php';
+
+
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+
+
+if (isset($_GET['code'])) {
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $client->setAccessToken($token['access_token']);
+
+    $google_oauth = new Google_Service_Oauth2($client);
+    $google_account_info = $google_oauth->userinfo->get();
+    $email =  $google_account_info->email;
+    $name =  $google_account_info->name;
+    ?>
+  <div class="input-user">
+      <label for="email">Email <?php echo $email; ?></label>
+      <label for="name">Name <?php echo $name; ?></label>
+    </div>
+    
+    <?php } else {?>
     <div class="container" id="signInForm">
         <h1 class="form-title">Sign In</h1>
         <form action="register.php" method="post">
@@ -30,7 +62,7 @@
             OR
         </p>
         <div class="icons">
-            <i class="fab fa-google"></i>
+           <a href="<?php echo $client->createAuthUrl()?>"><i class="fab fa-google"></i></a>
             <i class="fab fa-facebook"></i>
         </div>
         <div class="link">
@@ -39,5 +71,6 @@
         </div>
     </div>
     <script src="script.js"></script>
+    <?php } ?>
 </body>
 </html>
