d6dc021669b096afc4f1dbf4d87dd73f0118f552
Pranjal Singh <prancoder@gmail.com>
2025-01-24 23:21:47 +0530

    Added scripts for deployment

--- services/connect.php
+++ services/connect.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Load the .env file
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
@@ -10,9 +9,10 @@ $host = $_ENV['DB_HOST'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
 $db = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
-    echo "Failed to connect to DB" . $conn->connect_error;
+    die("Failed to connect to DB: " . $conn->connect_error);
 }
 ?>
