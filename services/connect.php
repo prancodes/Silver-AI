91f4d6b3bf242037e8567816be7d9436fd1135e3
Pranjal Singh <prancoder@gmail.com>
2025-03-17 20:15:17 +0530

    Setup Dotenv

--- services/connect.php
+++ services/connect.php
@@ -2,8 +2,12 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+// Only load .env file if it exists (useful for local development)
+$envPath = __DIR__ . '/../.env';
+if (file_exists($envPath)) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
 
 $host = $_ENV['DB_HOST'];
 $user = $_ENV['DB_USER'];
@@ -11,14 +15,17 @@ $pass = $_ENV['DB_PASS'];
 $db   = $_ENV['DB_NAME'];
 $port = $_ENV['DB_PORT'];
 
-// Get CA certificate path from env variable
-$ca_cert_path = '../certs/cert.pem';
+// Use absolute path for the CA certificate
+$ca_cert_path = __DIR__ . '/../certs/cert.pem';
 
 $mysqli = mysqli_init();
+
 // Set SSL using the CA certificate path
 mysqli_ssl_set($mysqli, NULL, NULL, $ca_cert_path, NULL, NULL);
 
+// Establish the SSL connection
 $mysqli->real_connect($host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
+
 if ($mysqli->connect_error) {
     die("Failed to connect to DB: " . $mysqli->connect_error);
 }
